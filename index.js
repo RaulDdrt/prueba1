ff564cec202f54094757697ffd8eb8c6f9ec411e
RaulDdrt <rauldiazdrt@gmail.com>
2023-03-14 23:52:19 +0000

    Mi primer commit

---
+++ index.js
@@ -0,0 +1,27 @@
+function suma (num1, num2){
+
+    console.log(num1 + num2)
+
+}
+
+function resta (num1, num2){
+
+    console.log(num1 - num2)
+}
+
+function mult (num1, num2){
+
+    console.log(num1 * num2)
+}
+
+function div (num1, num2){
+
+    console.log(num1 / num2)
+}
+
+function cua (num1){
+
+    console.log(num1*num1)
+}
+
+module.exports = {suma,resta,mult,div}
